7fadfd323b9c09c5e8119c98f3c2782cb31f45b9
Alex Yudi <94754085+alex-yudi@users.noreply.github.com>
2023-10-27 17:58:07 -0300

    Correção de variáveis de fonte

--- src/styles/themes/default.ts
+++ src/styles/themes/default.ts
@@ -8,75 +8,18 @@ export const defaultTheme = {
     brandColorPure: "#5F41D9",
     brandColorTealLight: "#99F3F5",
   },
+  // weight size/heigt family
   fonts: {
-    menuOptions: `
-      font-family: Inter;
-      font-size: 0.9375rem;
-      font-style: normal;
-      font-weight: 500;
-      line-height: 0.98438rem;
-    `,
-    menuClient: `
-      font-family: Inter;
-      font-size: 0.9375rem;
-      font-style: normal;
-      font-weight: 600;
-      line-height:  0.98438rem;`,
-    submenuTitle: `
-      font-family: sans-serif;
-      font-size: 0.9375rem;
-      font-style: normal;
-      font-weight: 600;
-      line-height: 1.17188rem;
-      letter-spacing: 0.00469rem;`,
-    submenuDescription: `
-      font-family: Inter;
-      font-size: 0.8125rem;
-      font-style: normal;
-      font-weight: 400;
-      line-height: 1.21875rem;
-    `,
-    tittleHat: `
-      font-family: sans-serif;
-      font-size: 1rem;
-      font-style: normal;
-      font-weight: 600;
-      line-height: 1.05rem ;
-      letter-spacing: 0.15rem;
-      text-transform: uppercase;`,
-    tittleH32: `
-      font-family: sans-serif;
-      font-size: 2rem;
-      font-style: normal;
-      font-weight: 400;
-      line-height: 2.5rem 
-      letter-spacing: 0.01rem;`,
-    tittleH20: `
-      font-family: sans-serif;
-      font-size: 1.25rem;
-      font-style: normal;
-      font-weight: 600;
-      line-height:  1.5625rem; 
-      letter-spacing: 0.00625rem;
-    `,
-    bodyText16: `
-      font-family: Inter;
-      font-size: 1rem;
-      font-style: normal;
-      font-weight: 400;
-      line-height: 1.5rem;`,
-    bodyLink: `
-      font-family: Inter;
-      font-size: 1rem;
-      font-style: normal;
-      font-weight: 600;
-      line-height: 1.25rem;`,
-    languageOptions: `
-      font-family: Inter;
-      font-size: 0.9375rem;
-      font-style: normal;
-      font-weight: 500;
-      line-height: 0.98438rem;`,
+    menuOptions: ` 500 0.9375rem/0.98438rem Inter`,
+    menuClient: `600 0.9375rem/0.98438rem Inter`,
+    submenuTitle: `600 0.9375rem/1.17188rem sans-serif`,
+    submenuDescription: `400 0.8125rem/1.21875rem Inter`,
+    tittleHat: `600 1rem/1.05rem sans-serif`,
+    tittleH32: `400 2rem/2.5rem sans-serif`,
+    tittleH20: `600 1.25rem/1.5625rem sans-serif; letter-spacing: 0.00625rem`,
+    bodyText16: `400 1rem/1.5rem Inter`,
+    bodyLink: `600 1rem/1.25rem Inter`,
+    languageOptions: `500 0.9375rem/0.98438rem Inter`,
 
   }
 } as const
