f8dec380b018b35daaa635cbcfc63ddaa6a05fd1
Alex Yudi <94754085+alex-yudi@users.noreply.github.com>
2023-10-29 19:45:27 -0300

    Conexão de axios com api em deploy na rede

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,8 @@
 import axios from 'axios'
 
+const localHost = 'http://localhost:3000'
+const alternativeApi = 'https://cute-tan-leopard-shoe.cyclic.app';
+
 export const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: alternativeApi,
 })
